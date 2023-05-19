f5eaf1da03d036b3fc45714ede5e7125a3d140b2
huyhuanhg <huyhuanhg@gmail.com>
2023-05-19 17:27:53 +0700

    improve suggest type quiz 3 and 4

--- src/components/Question/FillCharacterQuiz/FillCharacterQuiz.props.ts
+++ src/components/Question/FillCharacterQuiz/FillCharacterQuiz.props.ts
@@ -3,4 +3,5 @@ import VocabularyType from "@/types/entities/VocabularyType";
 export default interface FillCharacterQuizProps {
   vocabulary: VocabularyType;
   setAnswer: Function;
+  rate: number
 }
