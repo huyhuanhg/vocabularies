d2811b4b26209ef56e541d28c4f32cec513949c0
huyhuanhg <huyhuanhg@gmail.com>
2023-05-24 07:10:59 +0700

    update template msg

--- src/helpers/chat/message.ts
+++ src/helpers/chat/message.ts
@@ -5,7 +5,8 @@ export const sayHello = (name: string) => {
 }
 
 export const templateMessage = (content: string) => {
-  return `Bạn hãy ghi nhớ điều này trong những câu hỏi tiếp theo nếu tôi hỏi không liên quan về kiến thức chủ đề tiếng Anh bạn hãy từ chối bằng cách trả lời: "Vui lòng hỏi về chủ đề tiếng Anh!", chú ý không nhắc đến hoặc tranh luận thêm bất cứ điều gì về vấn đề này trong câu trả lời của bạn.
+  return `Bạn hãy ghi nhớ điều này trong những câu hỏi tiếp theo nếu tôi hỏi không liên quan về kiến thức chủ đề tiếng Anh bạn hãy từ chối bằng cách trả lời: "Vui lòng hỏi về chủ đề tiếng Anh!".
   Và xin chú ý cân nhắc kỹ những tin nhắn trước tránh trường hợp đưa ra câu trả lời sai lệch hoặc không thông minh.
+  Chú ý không nhắc đến, cảnh báo, nhắc nhở, đàm phán hay tranh luận thêm bất cứ gì về vấn đề này trong câu trả lời của bạn.
   ${content}`
 }
